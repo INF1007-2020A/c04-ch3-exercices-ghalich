627f342c66f369bdbeb16afa6919af181473faa0
ghalich <ghali.chraibi@polymtl.ca>
2020-09-14 23:05:16 +0000

    Auto commit

--- exercice.py
+++ exercice.py
@@ -17,6 +17,7 @@ def average(a: float, b: float, c: float) -> float:
 
 
 def to_radians(angle_degs: float, angle_mins: float, angle_secs: float) -> float:
+    deg_decimaux = Degrés + (Minutes + Secondes/60)/60
     return math.radians(angle_degs)
 
 
