2acf0de5b6b6313ecc87e8382cc49f9b38aaf970
ghalich <ghali.chraibi@polymtl.ca>
2020-09-14 23:27:12 +0000

    Auto commit

--- exercice.py
+++ exercice.py
@@ -21,7 +21,7 @@ def to_radians(angle_degs: float, angle_mins: float, angle_secs: float) -> float
 
 
 def to_degrees(angle_rads: float) -> tuple:
-    
+    resultat = math.degrees(an)
     return (math.a)
 
 
